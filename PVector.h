88569cc7fd86e7328cffada0996eaf15a650bd8b
Richard Barnes <rbarnes23@gmail.com>
2022-01-06 11:44:33 -0500

    Update PVector.h

--- PVector.h
+++ PVector.h
@@ -1,3 +1,12 @@
+/**
+ * @ Author: Richard Barnes
+ * @ Create Time: 2022-01-06 10:50:38
+ * @ Modified by: Richard Barnes
+ * @ Modified time: 2022-01-06 11:41:44
+ * @ Description:Adam Schiff Coding Train PVector Conversion for Navigation using Vector Math ie. Length and Direction of Vector 
+ */
+
+
 #ifndef __PVECTOR_H__
 #define __PVECTOR_H__
 
